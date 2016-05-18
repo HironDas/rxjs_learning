1f0d4667abfaad45754009c75ac23e9cc5a73726
Hiron Das <hiron_cse@yahoo.com>
2016-05-18 22:11:50 +0600

    message from office

--- src/code.js
+++ src/code.js
@@ -5,14 +5,19 @@ var quakes = Rx.Observable.create(function(observer) {
 	window.eqfeed_callback = function(response) {
 		quakes.forEach(function(quake) {
 			observer.onNext(quake);
+			observer.onCompleted();
 		});
 	};
 
 	map.loadJSONP(map.QUAKE_URL);
+
+}).flatMap(function transform(dataset){
+	return Rx.Observable.from(dataset.response.features);
 });
 
 
 quakes.subscribe(function(quake) {
+	console.log(quake);
 	var coords = quake.geometry.coordinates;
 	var size = quake.properties.mag * 10000;
 
