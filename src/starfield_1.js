3b62a2652e803a6ee26ecb1a1a6f82a3428afc06
Hiron Das <hiron_cse@yahoo.com>
2016-05-24 21:55:24 +0600

    the game is updating

--- src/starfield_1.js
+++ src/starfield_1.js
@@ -1,20 +1,31 @@
 var Rx = require('rx');
+var SpaceShip = require('./hero_1')
 
-var cnavas = document.createElement('canvas');
+var canvas = document.createElement('canvas');
 var ctx = canvas.getContext('2d');
 
-doucment.body.appendChild(canvas);
+document.body.appendChild(canvas);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var SPEED = 40;
 var STAR_NUMBER = 250;
 
+
+function paintStars(stars) {
+	ctx.fillStyle = '#000000';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = '#ffffff';
+	stars.forEach(function(star) {
+		ctx.fillRect(star.x, star.y, star.size, star.size);
+	});
+}
+
 var StarStream = Rx.Observable.range(1, STAR_NUMBER)
 	.map(function(){
 		return {
 			x: parseInt(Math.random() * canvas.width),
-			y: parseInt(Math.random() * convas.height),
+			y: parseInt(Math.random() * canvas.height),
 			size: Math.random() * 3 +1
 		};
 	})
@@ -29,5 +40,16 @@ var StarStream = Rx.Observable.range(1, STAR_NUMBER)
 			});
 			return starArray;
 		});
-	})
+	});
 
+var Game = Rx.Observable
+	.combineLatest(
+		StarStream, SpaceShip,
+		function(stars, spaceship) {
+			return {stars: stars, spaceship: spaceship};
+		});
+
+Game.subscribe(function(actors){
+	paintStars(actors.stars);
+	paintSpaceShip(actors.spaceship.x, actors.spaceship.y);
+});
