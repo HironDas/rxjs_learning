3ee0bbe51f49b723eac465bebbaa1dc5b4795a5f
Hiron Das <hcdas.09@gmail.com>
2016-06-07 05:05:45 +0600

    shots added

---
+++ src/hero_shots.js
@@ -0,0 +1,6 @@
+var playerFiring = Rx.Observable
+	.merge(
+	Rx.Observable.fromEvent(canvas, 'click'),
+	Rx.Observable.fromEvent(canvas, 'keydown')
+		.filter(function(evt){ return evt.keycode === 32; })
+	);
