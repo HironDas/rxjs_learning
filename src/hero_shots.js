3b94c75152a786f79b20fb68be0c964418b94817
Hiron Das <hiron_cse@yahoo.com>
2016-06-08 22:02:16 +0600

    now at page 61

--- src/hero_shots.js
+++ src/hero_shots.js
@@ -5,4 +5,15 @@ var playerFiring = Rx.Observable
 	Rx.Observable.fromEvent(canvas, 'click'),
 	Rx.Observable.fromEvent(canvas, 'keydown')
 		.filter(function(evt){ return evt.keycode === 32; })
-	);
+	).sample(200)
+	.timeStamp();
+
+var HeroShots = Rx.Observable.combineLatest(
+	playerFiring,
+	SpaceShip,
+	function(shotEvents, spaceShip){
+		return {x: spaceShip.x};
+	})
+	.scan(function(shorArray, shot){
+		shotArray.push({x: shot.x, y: HERO_Y});
+	},[]);
