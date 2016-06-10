bbf444d60fa6946579a65347660a63d13f0d3414
Hiron Das <hcdas.09@gmail.com>
2016-06-10 14:32:53 +0600

    nothing

--- src/hero_shots.js
+++ src/hero_shots.js
@@ -2,11 +2,11 @@ var Rx = require('rx');
 
 var playerFiring = Rx.Observable
 	.merge(
-	Rx.Observable.fromEvent(canvas, 'click'),
-	Rx.Observable.fromEvent(canvas, 'keydown')
+		Rx.Observable.fromEvent(canvas, 'click'),
+		Rx.Observable.fromEvent(canvas, 'keydown')
 		.filter(function(evt){ return evt.keycode === 32; })
 	).sample(200)
-	.timeStamp();
+	.timestamp();
 
 var HeroShots = Rx.Observable.combineLatest(
 	playerFiring,
