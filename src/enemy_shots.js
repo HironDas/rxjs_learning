312d3e4189801f6c6988d689d826aa7a8bcdb2fe
Hiron Das <hiron_cse@yahoo.com>
2016-06-13 21:56:45 +0600

    this is a begenning of enemy shots

---
+++ src/enemy_shots.js
@@ -0,0 +1,27 @@
+var Rx = require('rx');
+
+function isVisible(obj) {
+	return obj.x > -40 && obj.x < canvas.width + 40 &&
+			obj.y > -40 && obj.y < canvas.height + 40;
+}
+
+var ENEMY_FREQ = 15000;
+var ENEMY_SHOOTING_FREQ = 750;
+var Enemies = Rx.Observable.interval(ENEMY_FREQ)
+	.scan(function(enemyArray) {
+		var enemy = {
+			x: parseInt(Math.random() * canvas.width),
+			y: -30,
+			shots: []
+		};
+
+		Rx.Observable.interval(ENEMY_SHOOTING_FREQ)
+			.subscribe(function(){
+				enemy.shots.push({x: enemy.x, y: enemy.y });
+				enemy.shots = enemy.shots.filter(isVisible);
+			});
+
+		enemyArray.push(enemy);
+		return enemyArray.filter(isVisible);
+	}, []);
+	
