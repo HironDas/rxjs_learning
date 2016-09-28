9dc39f7459eda57390fa210cb0c9689866039710
Hiron Das <hiron_cse@yahoo.com>
2016-09-28 21:53:46 +0600

    some updated after a long time

--- src/schedulersTest.js
+++ src/schedulersTest.js
@@ -2,11 +2,35 @@ var Rx = require('rx');
 
 var arr = Array.from({length : 1000}, (d,i)=> i);
 
-var timeStart = Date.now();
+/*var timeStart = Date.now();
 Rx.Observable.from(arr, null, null, Rx.Scheduler.default).subscribe(
 	function onNext() {},
 	function onError() {},
 	function onCompleted(){
 		console.log('Total Time: '+(Date.now() - timeStart)+ 'ms');
 	});
-console.log('Hi this is I');
+console.log('Hi this is I');*/
+
+
+console.log('before subscription');
+
+Rx.Observable.range(1, 5)
+	.do((d)=> console.log('Processing value '+d))
+	.map((value)=> value*value)
+	.subscribe(function(value){
+		console.log("Emitted "+value);
+	});
+
+console.log('after subscription');
+
+console.log('before subscription');
+
+Rx.Observable.range(1, 5)
+	.do((d)=> console.log('Processing value '+d))
+	.observeOn(Rx.Scheduler.default)
+	.map((value)=> value*value)
+	.subscribe(function(value){
+		console.log("Emitted "+value);
+	});
+
+console.log('after subscription');
